8df7d3c2844ada39716f9758c233f017af490589
Matthew Philyaw <matthew.philyaw@gmail.com>
2012-11-26 19:42:31 -0500

    reworked message display... it's not pretty but works.

--- led_driver.c
+++ led_driver.c
@@ -1,11 +1,16 @@
 #include "led_driver.h"
 #include <pic16f1823.h>
 
-void set_led(u_char* pair) {
-    u_char flip = *pair & 0x80;
-    u_char lo = 0x01 << (0x0f & (*pair ^ 0x80));
-    u_char hi = 0x01 << ((*pair & 0x70) >> 4);
-    u_char mask = (hi | lo) ^ 0xff;
+void display_led(char* x, char* y)
+{
+    set_led(&grid[*x][*y]);
+}
+
+void set_led(char* pair) {
+    char flip = *pair & 0x80;
+    char lo = 0x01 << (0x0f & (*pair ^ 0x80));
+    char hi = 0x01 << ((*pair & 0x70) >> 4);
+    char mask = (hi | lo) ^ 0xff;
     TRISA = mask >> 6;
     TRISC = mask;
 
